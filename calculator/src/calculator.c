51756af0291665e05322b7250118594202e7c1db
Golov Dmitrii <diman66602@yandex.ru>
2022-05-09 12:08:42 +0300

    Калькулятор с Ввод-Выводом с текстового файла

--- calculator/src/calculator.c
+++ calculator/src/calculator.c
@@ -17,57 +17,56 @@ int main(int argc,char *argv[]) {
 	setvbuf(stderr, NULL, _IONBF, 0);
     float a,b,x,d;
     char c,r,q;
+    FILE *input,*output;
+	input = fopen ("input.txt","r");
+	output = fopen ("output.txt","w");
     do
     {
-    printf("Введите с каким калькулятором вы хотите работать Векторный-v Обычный-c:");
-         scanf(" %c",&q);
+         fscanf(input," %c",&q);
      switch(q)                         //Разделение на векторный и обычный калькулятор
      {
-     case 'c':                         //Обьчный калькулятор
-    printf("Список доступных команд:\n");//Список доступных команд
-    	      printf("Сложение +\n");
-    	      printf("Вычитание -\n");
-    	      printf("Умножение *\n");
-    	      printf("Деление /\n");
-    	      printf("Факториал числа !\n");
-    	      printf("Возведение в степень ^\n");
-    printf("Введите первое число:");   //Ввод переменных пользователем
-    scanf("%f",&a);
-    printf("Введите второе число:");
-    scanf("%f",&b);
-    printf("Введите желаемую операцию:");
-    scanf(" %c",&c);
+     case 's':                         //Обьчный калькулятор
+    fscanf(input,"%f ",&a);             //Ввод переменных пользователем
+    fprintf(output,"%f ",a);
+    fscanf(input,"%f",&b);
+    fscanf(input," %c",&c);
     switch(c)                      //Блок арифметичиских операций
       {
         case '+':                  // Сложение
-        printf("Результат сложения: %f\n", a+b);
+        fprintf(output,"+ %f ",b);
+        fprintf(output,"= %f\n", a+b);
         break;
         case '-':                 // Вычитание
-        printf("Результат вычитания: %f\n", a-b);
+        fprintf(output,"- %f ",b);
+        fprintf(output,"= %f\n", a-b);
         break;
         case '*':                 // Умножение
-        printf("Результат умножения: %f\n", a*b);
+        fprintf(output,"* %f ",b);
+        fprintf(output,"= %f\n", a*b);
         break;
         case '/':                 // Деление
-        printf("Результат деления:%f\n", a/b);
+        fprintf(output,"/ %f ",b);
+        fprintf(output,"= %f\n", a/b);
         break;
         case '!':                 // Факториал
+        fprintf(output,"! ");
         x=a;
         for(d=1;d<x;d++)
         {
         a=a*(x-d);
         }
         if (a<0){
-        printf("Вы берёте факториал отрицательно числа нужно ввести положительное");
+        fprintf(output,"Вы берёте факториал отрицательно числа нужно ввести положительное");
         }else{
-        printf("Результат вычисление факториала:%f\n",a);
+        fprintf(output,"= %f\n",a);
         }
         break;
         case '^':                 // Возведение в степень
-        printf("Результат возведения в степень:%f\n", pow(a,b));
+        fprintf(output,"^ %f ",b);
+        fprintf(output,"= %f\n", pow(a,b));
         break;
         default:                 // Сообщение о том что нет операции
-        printf("Такой операции нет введите доступную операцию");
+        fprintf(output,"Такой операции нет введите доступную операцию");
       }
 			break;
     case 'v':                   //Векторный калькулятор
@@ -75,43 +74,70 @@ int main(int argc,char *argv[]) {
     	double res;
     	int size;
     	char c;
-    	printf("Список доступных команд:\n");//Список доступных команд
-    	     printf("Сложение +\n");
-    	     printf("Вычитание -\n");
-    	     printf("Скалярное произведение *\n");
-    	printf("Введите размер векторов: ");
-    	scanf("%i",&size);
+    	fscanf(input,"%i",&size);
     	x=malloc(size*sizeof(int));
     	y=malloc(size*sizeof(int));
-    	printf("Введите первый вектор: ");    //Ввод переменных пользователем
-    	for(int i=0;i<size;i++) scanf("%f",&x[i]);
-    	printf("Введите второй вектор: ");
-    	for(int i=0;i<size;i++) scanf("%f",&y[i]);
-    	    printf("Введите желаемую операцию:");
-    	    scanf(" %c",&c);
+    	for(int i=0;i<size;i++)       //Ввод переменных
+    	 {
+    	  fscanf(input,"%f",&x[i]);
+    	     }
+    	fprintf(output,"( ");
+    	  for (int i = 0; i < size; i++)
+    	     {
+    	  fscanf(input," %f",&y[i]);   //Вывод переменной x
+    	      }
+    	   for (int i=0;i<size;i++)
+    	      {
+    	     	fprintf(output,"%f ", x[i]);
+    	                        }
+    	   fprintf(output,") ");
+    	     fscanf(input," %c",&c);
     	    switch(c)                      //Блок арифметичиских операций
     	          {
     	            case '+':                  // Сложение
-    	            printf("Сложение векторов: ");
-    	            for(int i=0;i<size;i++) printf("%f \n",x[i]+y[i]);
-    	            printf("\n");
+    	           fprintf(output,"+ (");                          //Вывод переменной y
+    	           for (int i=0;i<size;i++)
+    	                 {
+    	            fprintf(output," %f ", y[i]);
+    	                 }
+    	            fprintf(output,")");
+    	            fprintf(output," = ");
+    	            fprintf(output,"( ");
+    	            for(int i=0;i<size;i++) fprintf(output,"%f ",x[i]+y[i]);
+    	            fprintf(output,") ");
     	            break;
     	            case '-':                 // Вычитание
-    	            for(int i=0;i<size;i++) printf("%f \n",x[i]-y[i]);
-    	            printf("\n");
+    	            	fprintf(output,"- (");
+    	            for (int i=0;i<size;i++)
+    	                   {
+    	            fprintf(output," %f ", y[i]);
+    	                   }
+    	            fprintf(output,")");
+    	            fprintf(output," = ");
+    	            fprintf(output,"( ");
+    	            for(int i=0;i<size;i++) fprintf(output,"%f ",x[i]-y[i]);
+    	            fprintf(output,") ");
     	            break;
     	            case '*':                 // Скалярное произведение
+    	            	fprintf(output,"* (");
+    	             for (int i=0;i<size;i++)
+    	            	     {
+    	            fprintf(output," %f ", y[i]);
+    	                     }
+    	            fprintf(output,")");
+    	            fprintf(output," = ");
     	            for(int i=0;i<size;i++) res += x[i]*y[i];
-    	            printf("%lf",res);
+    	            fprintf(output,"%lf",res);
     	            break;
     	            default:                 // Сообщение о том что нет операции
-    	            printf("Такой операции нет введите доступную операцию");
+    	            fprintf(output,"Такой операции нет введите доступную операцию");
     	            free(x);
     	            free(y);
     	          }
      }
-    printf("Хотите продолжить? (y/n)"); // Запрос на повторное выполнение
-    scanf(" %c",&r);
+      fscanf(input," %c",&r);
     } while (r == 'y');
+    fclose(input);
+    fclose(output);
    return 0;
 }
